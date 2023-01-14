1aedd459355828cf220171d80948c124f29fc005
André Lashley <andre.lashley@gmail.com>
2023-01-14 22:33:54 +0000

    Configure gas estimation to use USD

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,7 @@ require("./tasks/block-number")
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 
 module.exports = {
     solidity: "0.8.8",
@@ -26,5 +27,10 @@ module.exports = {
     },
     gasReporter: {
         enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        // token: "BNB"
     },
 }
