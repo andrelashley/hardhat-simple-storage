625cad49104de416928b3f40a0179170b66c35cf
André Lashley <andre.lashley@gmail.com>
2023-01-14 22:13:26 +0000

    Configure localhost network

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,6 +15,10 @@ module.exports = {
         url: GOERLI_RPC_URL,
         accounts: [PRIVATE_KEY],
         chainId: 5,
+      },
+      localhost: {
+        url: "http://127.0.0.1:8545/",
+        chainId: 31337
       }
     },
     etherscan: {
