60ee32bd919389ba300aa315e6bc078fdddd53fb
André Lashley <andre.lashley@gmail.com>
2023-01-14 22:03:18 +0000

    Verify contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,7 @@ require("dotenv").config()
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
     solidity: "0.8.8",
@@ -14,5 +15,8 @@ module.exports = {
         accounts: [PRIVATE_KEY],
         chainId: 5,
       }
+    },
+    etherscan: {
+      apiKey: ETHERSCAN_API_KEY
     }
 }
